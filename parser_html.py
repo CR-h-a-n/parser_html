40adc546e1c1b721424cdb10e8cc0d1ca4d9ab27
CR-h-a-n <Foksic2006@yandex.ru>
2022-07-21 19:14:13 +0300

    parser

---
+++ parser_html.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+import json
+import datetime
+
+
+def parser_news(class_string):
+    all_tr_news_set = soup.findAll('tr', class_=class_string)
+    for one_tr_news in all_tr_news_set:
+        time_news = one_tr_news.find('td', class_='first left time js-time')
+        country = one_tr_news.find('td', class_='left flagCur noWrap')
+        importance = one_tr_news.findAll('i', class_='grayFullBullishIcon')
+        event_tag_td = one_tr_news.find('td', class_='left event')
+        event_tag_a = event_tag_td.find('a')
+        list_news.append({'time': time_news.text, 'country': country.text[-3:], 'importance': len(importance),
+                          'event': event_tag_a.text[1:]})
+
+
+url = 'https://ru.investing.com/economic-calendar/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+list_news = []
+
+parser_news('js-event-item')
+pprint.pprint(list_news)
+
+result = json.dumps(list_news)
+file_name = 'economic_calendar ' + str(datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")) + '.json'
+with open(file_name, 'w') as f:
+    json.dump(result, f)
+
